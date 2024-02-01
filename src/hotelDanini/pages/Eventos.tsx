f562dd960264e20c49e6d47baf946cb67d1b1f84
Arturo Riveros <53869372+RiverosArturo@users.noreply.github.com>
2024-01-31 22:04:35 -0600

    Update Eventos.tsx

--- src/hotelDanini/pages/Eventos.tsx
+++ src/hotelDanini/pages/Eventos.tsx
@@ -90,8 +90,6 @@ export const Eventos = () => {
                     ))}
                   </div>
                 </div>
-
-                {/* <div className="col-md-12"></div> */}
               </div>
             </div>
           </div>
