fa587998ca90c935ccb87152c1c44f4bdcdd1a7f
Arturo Riveros <53869372+RiverosArturo@users.noreply.github.com>
2024-05-06 22:21:38 -0600

    Update KingSize.tsx

--- src/hotelDanini/pages/habitaciones/KingSize.tsx
+++ src/hotelDanini/pages/habitaciones/KingSize.tsx
@@ -3,25 +3,28 @@ import { Habitacion, Tabla } from "../../components";
 const habitacion = {
   nombre: "King Size",
   informacion:
-    "Habitación King Size con vista a la naturaleza que rodea nuestro hotel, ideal para usted y su pareja, decorada al estilo colonial.",
+    "Confortable habitación con una cama king, cuentan con aire acondicionado, calefacción, televisión, teléfono, amenidades, secadora de pelo, burro de planchar y plancha a solicitud, con acceso directo al jardín de la fuente o con balcón para disfrutar de una hermosa vista a la montaña.",
   imgs: [
     "https://res.cloudinary.com/dawwp31sm/image/upload/v1694402577/habitaciones/kingSize/king2_tgkkop.jpg",
     "https://res.cloudinary.com/dawwp31sm/image/upload/v1694402575/habitaciones/kingSize/king_yynfvt.jpg",
     "https://res.cloudinary.com/dawwp31sm/image/upload/v1694402573/habitaciones/kingSize/ba%C3%B1o_fwyafj.jpg",
   ],
-  precio: 1200,
+  // precio: 1200,
   cupo: 2,
 };
 
 const tabla = {
   elemento: [
-    "Cama king size",
-    'Pantalla 40"',
+    "Cama King size",
+    "Aire acondicionado",
+    "Secadora de pelo",
+    'Pantalla de TV de 40"',
     "Internet inalámbrico",
-    "Balcón",
-    "Calefacción",
-    "Secadora de cabello",
     "Room service",
+    "Teléfono",
+    "Burro de planchar a solicitud",
+    "Plancha a solicitud",
+    "Vista a la montaña"
   ],
 };
 
