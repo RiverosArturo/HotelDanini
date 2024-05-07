7818a029410ba9484b1b559a6d40bd43a9d6db00
Arturo Riveros <53869372+RiverosArturo@users.noreply.github.com>
2024-05-06 22:22:55 -0600

    Update QueenSize.tsx

--- src/hotelDanini/pages/habitaciones/QueenSize.tsx
+++ src/hotelDanini/pages/habitaciones/QueenSize.tsx
@@ -3,25 +3,28 @@ import { Habitacion, Tabla } from "../../components";
 const habitacion = {
   nombre: "Queen Size",
   informacion:
-    "Habitación Queen Size con vista a la naturaleza que rodea nuestro hotel, ideal para usted y su pareja, decorada al estilo colonial.",
+    "Confortable habitación con una cama queen, cuenta con aire acondicionado, calefacción, televisión, teléfono, amenidades, secadora de pelo, burro de planchar y plancha a solicitud, con acceso directo al jardín de la fuente o con balcón para disfrutar de una agradable vista a la montaña.",
   imgs: [
     "https://res.cloudinary.com/dawwp31sm/image/upload/v1694402655/habitaciones/queenSize/queen2_xe497t.jpg",
     "https://res.cloudinary.com/dawwp31sm/image/upload/v1694402652/habitaciones/queenSize/queen_ay0ymz.jpg",
     "https://res.cloudinary.com/dawwp31sm/image/upload/v1694402650/habitaciones/queenSize/ba%C3%B1o_etyjbf.jpg",
   ],
-  precio: 900,
+  // precio: 900,
   cupo: 2,
 };
 
 const tabla = {
   elemento: [
     "Cama queen size",
-    'Pantalla 40"',
+    "Aire acondicionado",
+    "Secadora de pelo",
+    'Pantalla de TV de 40"',
     "Internet inalámbrico",
-    "Balcón",
-    "Calefacción",
-    "Secadora de cabello",
     "Room service",
+    "Teléfono",
+    "Burro de planchar a solicitud",
+    "Plancha a solicitud",
+    "Vista a la montaña"
   ],
 };
 
