8094c1446f40d590335bd645d0a5864661bc6ae8
Arturo Riveros <53869372+RiverosArturo@users.noreply.github.com>
2024-05-06 22:19:59 -0600

    Update DobleMatrimonial.tsx

--- src/hotelDanini/pages/habitaciones/DobleMatrimonial.tsx
+++ src/hotelDanini/pages/habitaciones/DobleMatrimonial.tsx
@@ -3,25 +3,28 @@ import { Habitacion, Tabla } from "../../components";
 const habitacion = {
   nombre: "Doble Matrimonial",
   informacion:
-    "Habitación Doble Matrimonial con vista a la naturaleza que rodea nuestro hotel, ideal para usted y su familia, decorada al estilo colonial.",
+    "Confortable habitación con dos camas matrimoniales, todas las habitaciones cuentan con aire acondicionado, calefacción, televisión, teléfono, amenidades, secadora de pelo, burro de planchar y plancha a solicitud, con acceso directo al jardín de la fuente o con balcón para disfrutar de una agradable vista a la montaña.",
   imgs: [
     "https://res.cloudinary.com/dawwp31sm/image/upload/v1694402545/habitaciones/dobleMatrimonial/doble2_jcfxbb.jpg",
     "https://res.cloudinary.com/dawwp31sm/image/upload/v1694402543/habitaciones/dobleMatrimonial/doble_x6qnoa.jpg",
     "https://res.cloudinary.com/dawwp31sm/image/upload/v1694402541/habitaciones/dobleMatrimonial/ba%C3%B1o2_tdvdam.jpg",
   ],
-  precio: 1600,
+  // precio: 1600,
   cupo: 4,
 };
 
 const tabla = {
   elemento: [
     "2 camas matrimoniales",
-    'Pantalla 40"',
+    "Aire acondicionado",
+    "Secadora de pelo",
+    'Pantalla de TV de 40"',
     "Internet inalámbrico",
-    "Balcón",
-    "Calefacción",
-    "Secadora de cabello",
     "Room service",
+    "Teléfono",
+    "Burro de planchar a solicitud",
+    "Plancha a solicitud",
+    "Vista a la montaña"
   ],
 };
 
