8bb8b987022c0da0020762d4fe40cdb232f5d4b8
Arturo Riveros <53869372+RiverosArturo@users.noreply.github.com>
2024-05-06 22:20:37 -0600

    Update index.ts

--- src/hotelDanini/pages/habitaciones/index.ts
+++ src/hotelDanini/pages/habitaciones/index.ts
@@ -1,5 +1,5 @@
 export * from './DobleMatrimonial';
 export * from './KingSize';
-export * from './MasterDeluxe';
+export * from './MasterSuite';
 export * from './QueenSize';
 export * from './SuiteDeluxe';
